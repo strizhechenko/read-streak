1df6d5077e13b9f35b6b84ef001a7fe92b618ebf
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2019-01-12 20:32:15 +0500

    Fixed: date format in tsv2csv tool

--- tsv2csv.py
+++ tsv2csv.py
@@ -6,6 +6,10 @@ with open('x.txt', 'r') as fd:
         words = line.split()
         date = words[0]
         day, month, year = date.split('.')
+        if int(month) < 10:
+            month = '0' + month
+        if int(day) < 10:
+            day = '0' + day
         date = "-".join([year, month, day])
         pages = sum(map(int, words[1:]))
         print(",".join([date, str(pages)]))
